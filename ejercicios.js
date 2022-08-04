3fbb60076030e8c2513f72536cfa1c01f300b35f
Luis Correa <luiscorrea874@gmail.com>
2022-08-04 13:27:36 -0500

    Primer Ejercicio

---
+++ ejercicios.js
@@ -0,0 +1,12 @@
+
+function ejercio_uno(){
+    let parte_uno = ((5+2-5)**2*5);
+    let parte_dos = 8/2-30;  
+    let parte_tres= 2*5-3;
+    let y = (parte_uno+parte_dos)/parte_tres;
+    return y; 
+}
+console.log(ejercio_uno());
+
+
+
