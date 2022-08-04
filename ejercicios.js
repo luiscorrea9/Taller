ff77512a0f0074f36a2521fd2543f5f8c5a2794e
Luis Correa <luiscorrea874@gmail.com>
2022-08-04 14:32:37 -0500

    Segundo Ejercicio

--- ejercicios.js
+++ ejercicios.js
@@ -8,5 +8,19 @@ function ejercio_uno(){
 }
 console.log(ejercio_uno());
 
+function ejercicio_dos(){
+    let z = 5;
+    let n = 3;
+    let m = z-n;
+    let parte_uno =(z+2-n)**2*m+8/2-30;
+    let parte_dos =2*5-3
+    let parte_tres= (parte_uno/parte_dos)**5;
+    let parte_cuatro=15*3-9/3
+    let y = parte_tres+parte_cuatro
+    return y;
+}
+console.log(ejercicio_dos());
+
+
 
 
